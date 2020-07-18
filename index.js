85654d98dc1bc6803c74152031659cb40558696a
stefancostin <stefanfcostin@gmail.com>
2020-07-18 22:10:29 +0300

    Add authentication

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 require("dotenv").config({ path: __dirname + "/src/config/.env" });
+require("./src/models/user");
 require("./src/services/passport");
 
+const cookieSession = require("cookie-session");
 const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+const authRoutes = require("./src/routes/auth-routes");
+
+mongoose.connect(process.env.MONGO_DB_URI);
 
 const app = express();
-require("./src/routes/auth-routes")(app);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [process.env.COOKIE_KEY],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- src/services/passport.js
+++ src/services/passport.js
@@ -1,6 +1,20 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  // for authorization the MongoDB generated id is used
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((existingUser) => {
+    done(null, existingUser);
+  });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -9,9 +23,17 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken: ", accessToken);
-      console.log("refreshToken: ", refreshToken);
-      console.log("profile: ", profile);
+      const user = { googleId: profile.id };
+
+      User.findOne(user).then((existingUser) => {
+        if (existingUser) {
+          // we already have a record with the given profile ID
+          done(null, existingUser);
+        } else {
+          // we don't have a user record with this ID, make a new record
+          new User(user).save().then((createdUser) => done(null, createdUser));
+        }
+      });
     }
   )
 );
